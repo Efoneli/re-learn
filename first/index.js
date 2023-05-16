e2a69b5f67f4499df5a47b4fd632887c6d7289b7
efoneli <abbaefoneli@gmail.com>
2023-05-16 10:29:57 +0100

    first

--- first/index.js
+++ first/index.js
@@ -8,7 +8,17 @@
 //     console.log('Good evening');
 // }
 
-for (let i = 0; i < 5; i++) {
-    console.log('hello world')
+// for (let i = 0; i < 5; i++) {
+//     if (i % 2 !== 0) console.log(i)
+//     console.log('hello world', i)
+// }
+
+
+
+let i = 9;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
 }
+
 
