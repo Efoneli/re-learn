a493abe9fd76b582bdd71bc8c087057976be634a
efoneli <abbaefoneli@gmail.com>
2023-05-16 10:10:55 +0100

    first

--- first/index.js
+++ first/index.js
@@ -1,9 +1,13 @@
-let hour = 10;
+// let hour = 10;
 
-if (hour >= 6 && hour < 12) {
-    console.log('Good morning');
-}else if (hour >= 12 && hour < 6) {
-    console.log('Good afternoon');
-}else {
-    console.log('Good evening');
+// if (hour >= 6 && hour < 12) {
+//     console.log('Good morning');
+// }else if (hour >= 12 && hour < 6) {
+//     console.log('Good afternoon');
+// }else {
+//     console.log('Good evening');
+// }
+
+for (let i = 0; i < 5; i++) {
+    console.log('hello world')
 }
