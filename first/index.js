96814c0759964b00e088148108203654c8f23314
efoneli <abbaefoneli@gmail.com>
2023-05-16 11:30:36 +0100

    first

--- first/index.js
+++ first/index.js
@@ -15,10 +15,50 @@
 
 
 
-let i = 9;
-while (i <= 5) {
-    if (i % 2 !== 0) console.log(i);
-    i++;
-}
+// let i = 9;
+// while (i <= 5) {
+//     if (i % 2 !== 0) console.log(i);
+//     i++;
+// }
+
+
+// FOR IN
+
+// we use for in when we want to access the property of an object
+// const person = {
+//     name: 'Sarah',
+//     age: 16,
+//     likes: 'chicken and chips'
+// }
+
+// for (let key in person) 
+// console.log(key, person[key])
+
+
+// const colors = ['red', 'pink', 'yellow']
+
+// for (let index in colors)
+
+// console.log(index, colors[index])
+
+
+// FOR OF
+// its used to iterate over the elements in an array
+
+// for (let color of colors) 
+// console.log(color)
+
+// let num = numbers(15,6)
+// console.log(num)
+
+// function numbers(a, b) {
+// return (a > b) ? a : b
+// }
+
+// let measure = isLandscape(26, 233)
+// console.log(measure)
+// function isLandscape(width, height) {
+//     return (width > height)
+// }
 
 
