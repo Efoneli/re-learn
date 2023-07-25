241d34db19fdbdfdce5accb4d0723cd97a04d0cc
efoneli <abbaefoneli@gmail.com>
2023-07-25 13:45:54 +0100

    test

--- arrays/index.js
+++ arrays/index.js
@@ -1,18 +1,72 @@
-let radius = 1
-let x = 1
-let y = 1
 
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('drawing')
-        }
-    }
+const now = new Date()
+const  date = new Date('July 25 2023 09:00')
+
+console.log(now.toDateString())
+
+let address = {
+    street: '123 Main St',
+    city: 'New York',
+    zipCode: '10001'
 }
 
-const circle = createCircle(3)
-console.log(circle)
+// function showAddress(address) {
+//    return `${address.street} ${address.city}, ${address.zipCode}`
+// }
 
-const circle2 = createCircle(2)
-console.log(circle.draw())
+// console.log(showAddress(address) )
+
+// // factory Function
+
+// function createAddress(street, city, zipCode) {
+//     return {
+//         street,
+//         city,
+//         zipCode
+//     }
+// }
+
+let address1 = new Address('a', 'b', 'c')
+let address2 = new Address('a', 'b', 'd')
+
+
+// constructor Function
+
+function Address(street, city, zipCode) {
+    this.street = street
+    this.city = city
+    this.zipCode = zipCode
+}
+
+function areEqual(address1, address2) {
+    if (address1 === address2)
+    return true
+}
+
+function areSame(address1, address2) {
+    if (Address.address1 === Address.address2)
+    return true
+}
+
+function areEqual(address1, address2) {
+    if (Address.address1 === Address.address2)
+    return true
+}
+
+console.log(areSame())
+console.log(areEqual())
+
+
+const blogPost= {
+    title: 'Hello',
+    body: 'Im enjoying this blog',
+    author: 'macRobinson',
+    comments: [
+        {'Liam': 'wonderful', 
+        'Sarah': 'beautiful',
+}],
+isLive: true,
+
+}
+
+function
